9cdeafea8de4c834051dc364a4a41527be171b47
Alex A <Alexapakidze@yahoo.com>
2023-08-04 17:59:42 -0600

    created Spring Boot Rest Project

---
+++ HvacPm/src/main/java/com/skilldistillery/hvacpm/HvacPmApplication.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.hvacpm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HvacPmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HvacPmApplication.class, args);
+	}
+
+}
---
+++ HvacPm/src/test/java/com/skilldistillery/hvacpm/HvacPmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.hvacpm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HvacPmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
